81073a15de2b8dd363828aee5d6df1b369f519e6
0x5341 <107183580+0x5341@users.noreply.github.com>
2025-11-18 08:28:00 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -57,4 +57,15 @@
         };
       }
     );
+  
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+};
 }
