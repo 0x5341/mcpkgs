5098f1f9dd9d107b9fcbcdce61319a03c5e0ced1
0x5341 <107183580+0x5341@users.noreply.github.com>
2025-11-18 08:22:07 +0900

    add update actions

--- chrome-devtools.nix
+++ chrome-devtools.nix
@@ -2,7 +2,7 @@
   chrome-devtools-src = inputs.chrome-devtools;
 in pkgs.buildNpmPackage (final: {
   pname = "chrome-devtools-mcp";
-  version = "master";
+  version = "chrome-devtools-mcp-v0.10.1";
   src = chrome-devtools-src;
   npmDepsHash = "sha256-T/R2h7WqR7L4jN8QjxB641MgjRr44DA3qlDgjD/c10k=";
   npmBuildScript = "bundle";
