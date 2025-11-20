fef42aa5acfcd024e6229ac1066c3f1a872a64ad
0x5341 <107183580+0x5341@users.noreply.github.com>
2025-11-20 01:55:03 +0000

    Update *.lock

--- chrome-devtools.nix
+++ chrome-devtools.nix
@@ -2,9 +2,9 @@
   chrome-devtools-src = inputs.chrome-devtools;
 in pkgs.buildNpmPackage (final: {
   pname = "chrome-devtools-mcp";
-  version = "chrome-devtools-mcp-v0.10.1";
+  version = "chrome-devtools-mcp-v0.10.2";
   src = chrome-devtools-src;
-  npmDepsHash = "sha256-T/R2h7WqR7L4jN8QjxB641MgjRr44DA3qlDgjD/c10k=";
+  npmDepsHash = "sha256-Z95RGUjkzreYGjW/ZCHK2pibpMpzD3YDK7Xx3OuaB3k=";
   npmBuildScript = "bundle";
   PUPPETEER_SKIP_DOWNLOAD = "1";
 })
